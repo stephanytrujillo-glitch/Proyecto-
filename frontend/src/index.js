1c3bdfb5fe76e0c41a8b140b1d09f29115c5e245
juanacuna07 <juan.acuna1@estudiantesunibague.edu.co>
2026-05-24 20:41:47 -0500

    fix: actualizar Google OAuth client ID

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,7 +7,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId="286409835143-4sah4eie4bgieuke968q3t08dn46qnf9.apps.googleusercontent.com">
+    <GoogleOAuthProvider clientId="286409835143-hlbpjmnkl4re90to594mni71ft05krgl.apps.googleusercontent.com">
       <App />
     </GoogleOAuthProvider>
   </React.StrictMode>
